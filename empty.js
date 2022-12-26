6800495679291d549d6b8a874c3424bb10f1876a
Roman <tramplin900@gmail.com>
2022-12-26 21:54:01 +0500

    homework (new,html and empty.js

--- empty.js
+++ empty.js
@@ -1,1 +1,21 @@
-const leftAndRight = 'get in book';
+let money = prompt('Ваш бюджет на месяц?', ''),
+	 time  = prompt('Введите дату в формате YYYY-MM-DD', '');
+	 
+let appData = {
+	budget            : money,
+	timeData          : time,
+	expenses          : '',
+	optionalExpenses  : {},
+	income            : [],
+	savings           : false
+};
+
+let questionOne =    prompt("Введите обязательную статью расходов в этом месяце", ''),
+	 questionTwo =    prompt("Во сколько обойдется?", ''),
+	 questionThree =  prompt("Введите обязательную статью расходов в этом месяце", ''),
+	 questionFour =   prompt("Во сколько обойдется?", '');
+	 
+appData.expenses.questionOne = questionTwo;
+appData.expenses.questionThree = questionFour;
+
+alert(appData.budget / 30);
